adb5fe7a012aa698319d3aec1af99631d565d84a
Sean Harrison <sah@bookgenesis.com>
2019-08-10 22:14:55 -0500

    0.2.0 Revised SQL and Dialects — bind the parameter data to SQL.render() and return from that the parameterized query string and values according to the given dialect.

--- sqly/dialects.py
+++ sqly/dialects.py
@@ -1,10 +1,44 @@
 from enum import Enum
+import re
+
 
 class Dialects(Enum):
-    EMBEDDED = ':varname'
-    MYSQL = '%(varname)s'
-    SQLITE = '?' 
+    # the value of each Dialect represents the syntax that it uses for query parameters
+    EMBEDDED = ':field'
+    MYSQL = '%(field)s'
+    SQLITE = '?'
     POSTGRES = '$i'
+
     ASYNCPG = POSTGRES
     PSYCOPG2 = MYSQL
     SQLALCHEMY = EMBEDDED
+
+    def render(self, query_string, data):
+        """render a query_string and its parameter values for this dialect."""
+        pattern = r":(\w+)\b"
+        fields = []
+
+        def replace(match):
+            field = match.group(1)
+            if field not in fields:
+                fields.append(field)
+
+            if self is self.EMBEDDED:
+                return f":{field}"
+            elif self is self.MYSQL:
+                return f"%({field})s"
+            elif self is self.SQLITE:
+                return "?"
+            elif self is self.POSTGRES:
+                return f"${len(fields)}"
+            else:
+                raise ValueError('Dialect %r not in Dialects' % self.dialect)
+
+        rendered_query_string = re.sub(pattern, replace, query_string)
+
+        if self in [self.EMBEDDED, self.MYSQL]:
+            parameter_values = {field: data[field] for field in fields}
+        elif self in [self.SQLITE, self.POSTGRES]:
+            parameter_values = [data[field] for field in fields]
+
+        return rendered_query_string, parameter_values
--- sqly/sql.py
+++ sqly/sql.py
@@ -7,8 +7,11 @@ from .lib import walk_list
 class SQL:
     query: list = field(default_factory=list)
     dialect: str = Dialects.EMBEDDED
-    fields: list = field(default_factory=list)
     keys: list = field(default_factory=list)
+    fields: list = field(default_factory=list)
+    filters: list = field(default_factory=list)
+    assigns: list = field(default_factory=list)
+    params: list = field(default_factory=list)
 
     def __post_init__(self):
         # allow the query to be a single string
@@ -18,44 +21,28 @@ class SQL:
     def __str__(self):
         return self.render()
 
-    def render(self):
+    def render(self, data):
+        """render the query and its values for a given data input."""
         query_string = ' '.join([str(q) for q in walk_list(self.query)])
-        return query_string.format(
-            keys=', '.join(self.keys),
-            filters=' AND '.join(self.assignments(self.keys)),
-            fields=', '.join(self.fields),
-            vars=', '.join(self.vars(self.fields)),
-            updates=', '.join(self.assignments(self.fields)),
+        fields = self.fields or data.keys()
+        rendered_query, parameter_values = self.dialect.render(
+            query_string.format(
+                keys=', '.join(self.keys),
+                fields=', '.join(fields),
+                filters=' AND '.join(self.filters or self.assigns_list(self.keys)),
+                assigns=', '.join(self.assigns or self.assigns_list(fields)),
+                params=', '.join(self.params or self.params_list(fields)),
+            ),
+            data,
         )
+        return rendered_query, parameter_values
 
-    def vars(self, fields=None):
+    def params_list(self, fields=None):
         if not fields:
             fields = self.fields
+        return [":%s" % field for field in fields]
 
-        if self.dialect not in Dialects:
-            raise ValueError('Dialect %r not in Dialects' % self.dialect)
-        if self.dialect == Dialects.POSTGRES:
-            return ["$%d" % index for index in range(1, len(fields) + 1)]
-        if self.dialect == Dialects.MYSQL:
-            return ["%%(%s)s" % field for field in fields]
-        if self.dialect == Dialects.EMBEDDED:
-            return [":%s" % field for field in fields]
-        if self.dialect == Dialects.SQLITE:
-            return ["?" for field in fields]
-
-    def assignments(self, fields=None):
+    def assigns_list(self, fields=None):
         if not fields:
             fields = self.fields
-
-        if self.dialect not in Dialects:
-            raise ValueError('Dialect %r not in Dialects' % self.dialect)
-        if self.dialect == Dialects.EMBEDDED:
-            return ["%s=:%s" % (field, field) for field in fields]
-        if self.dialect == Dialects.POSTGRES:
-            return [
-                "%s=$%d" % (field, list(fields).index(field) + 1) for field in fields
-            ]
-        if self.dialect == Dialects.MYSQL:
-            return ["%s=%%(%s)s" % (field, field) for field in fields]
-        if self.dialect == Dialects.SQLITE:
-            return ["%s=?" % field for field in fields]
+        return ["%s=:%s" % (field, field) for field in fields]
