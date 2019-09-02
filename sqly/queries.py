089b77473e7fddfd759d2d3d017dc3bf2d8c8577
Sean Harrison <sah@bookgenesis.com>
2019-09-02 11:20:51 -0500

    sqly.queries – some basic (python) prepared queries: select, insert, update, delete, upsert

---
+++ sqly/queries.py
@@ -0,0 +1,32 @@
+"""
+A library of prepared queries (in Python, not the database) for use by other programs.
+"""
+
+from .sql import SQL
+
+
+def select(dialect=None):
+    return SQL(['select {fields} from {table} {where}'], dialect=dialect)
+
+
+def insert(dialect=None):
+    return SQL(['insert into {table} ({fields}) values ({params})'], dialect=dialect)
+
+
+def update(dialect=None):
+    return SQL(['update {table} set {assigns} {where}'], dialect=dialect)
+
+
+def delete(dialect=None):
+    return SQL(['delete from {table} {where}'], dialect=dialect)
+
+
+def upsert(dialect=None):
+    return SQL(
+        [
+            'INSERT INTO {table} ({fields}) VALUES ({params})',
+            'ON CONFLICT ({keys}) DO UPDATE SET {assigns_excluded}',
+            'RETURNING *',
+        ],
+        dialect=dialect,
+    )
