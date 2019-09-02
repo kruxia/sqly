c706da270acbf35f9170d93516e1150facc5a7e0
Sean Harrison <sah@bookgenesis.com>
2019-09-02 11:58:13 -0500

    sqly.dialects – added OuputFormats enum, then each Dialect has its own name, and has an .output_formats property, so now we can .render() Dialects based on output_format rather than name, which is easier to read / understand.

--- sqly/dialects.py
+++ sqly/dialects.py
@@ -3,57 +3,93 @@ import re
 from enum import Enum
 
 
+class OutputFormats(Enum):
+    COLON_PARAM = ':field'
+    STR_PARAM = '%(field)s'
+    QMARK = '?'
+    DOLLAR_POS = '$i'
+
+
 class Dialects(Enum):
     # the value of each Dialect represents the syntax that it uses for query parameters
-    EMBEDDED = ':field'
-    MYSQL = '%(field)s'
-    SQLITE = '?'
-    POSTGRES = '$i'
+    EMBEDDED = 'embedded'
+    MYSQL = 'mysql'
+    SQLITE = 'sqlite'
+    POSTGRES = 'postgres'
+    ASYNCPG = 'asyncpg'
+    PSYCOPG2 = 'psycopg2'
+    SQLALCHEMY = 'sqlalchemy'
 
-    ASYNCPG = POSTGRES
-    PSYCOPG2 = MYSQL
-    SQLALCHEMY = EMBEDDED
+    @property
+    def output_format(self):
+        return {
+            self.EMBEDDED: OutputFormats.COLON_PARAM,
+            self.SQLALCHEMY: OutputFormats.COLON_PARAM,
+            self.MYSQL: OutputFormats.STR_PARAM,
+            self.PSYCOPG2: OutputFormats.STR_PARAM,
+            self.SQLITE: OutputFormats.QMARK,
+            self.POSTGRES: OutputFormats.DOLLAR_POS,
+            self.ASYNCPG: OutputFormats.DOLLAR_POS,
+        }[self]
 
     def render(self, query_string, data):
         """render a query_string and its parameter values for this dialect."""
         pattern = r"(?<!\\):(\w+)\b"
-        fields = []
+        fields = []  # ordered list of fields for positional outputs
 
         def replace_parameter(match):
             field = match.group(1)
-            if self in [self.SQLITE, self.POSTGRES] or field not in fields:
+
+            # Build the ordered fields list for positional outputs
+            if (
+                self.output_format in [OutputFormats.QMARK, OutputFormats.DOLLAR_POS]
+                or field not in fields
+            ):
                 fields.append(field)
 
-            if self is self.EMBEDDED:
+            # Return the field formatted for the output type
+            if self.output_format == OutputFormats.COLON_PARAM:
                 return f":{field}"
-            elif self is self.MYSQL:
+            elif self.output_format == OutputFormats.STR_PARAM:
                 return f"%({field})s"
-            elif self is self.SQLITE:
+            elif self.output_format == OutputFormats.QMARK:
                 return "?"
-            elif self is self.POSTGRES:
+            elif self.output_format == OutputFormats.DOLLAR_POS:
                 return f"${len(fields)}"
             else:
-                raise ValueError('Dialect %r not in Dialects' % self.dialect)
+                raise ValueError(
+                    'Dialect %r output_format %r not supported'
+                    % (self, self.output_format)
+                )
 
-        if self is self.MYSQL:
+        # 1. Escape string parameters in the STR_PARAM output format
+        if self.output_format == OutputFormats.STR_PARAM:
             # any % must be intended as literal and must be doubled
             query_string = query_string.replace('%', '%%')
 
+        # 2. Replace the parameter with its dialect-specific representation
         rendered_query_string = re.sub(pattern, replace_parameter, query_string).strip()
 
-        if self is not self.EMBEDDED:
+        # 3. Un-escape remaining escaped colon params
+        if self.output_format == OutputFormats.COLON_PARAM:
             # replace \:word with :word because the colon-escape is no longer needed.
             rendered_query_string = re.sub(r"\\:(\w+)\b", r":\1", rendered_query_string)
 
-        if self in [self.EMBEDDED, self.MYSQL]:
+        # 4. Build the parameter_values dict or list for use with the query
+        if self.output_format in [OutputFormats.COLON_PARAM, OutputFormats.STR_PARAM]:
+            # parameter_values is a dict of fields
             parameter_values = {
                 key: json.dumps(val) if isinstance(val, dict) else val
                 for key, val in {field: data[field] for field in fields}.items()
             }
-        elif self in [self.SQLITE, self.POSTGRES]:
+        elif self.output_format in [OutputFormats.QMARK, OutputFormats.DOLLAR_POS]:
             parameter_values = [
                 json.dumps(val) if isinstance(val, dict) else val
                 for val in [data[field] for field in fields]
             ]
+        else:
+            raise ValueError(
+                'Dialect %r output_format %r not supported' % (self, self.output_format)
+            )
 
         return rendered_query_string, parameter_values
