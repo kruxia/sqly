0e09c0c02ad2ad93e82bcbae477e95aede0a0c9b
Sean Harrison <sah@bookgenesis.com>
2019-12-26 23:01:35 -0600

    0.4.5 – installl_requires from req/install.txt

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import json
 import os
+import re
 from codecs import open
 
 from setuptools import find_packages, setup
@@ -12,11 +13,22 @@ with open(os.path.join(PATH, "setup.json"), encoding='utf-8') as f:
 with open(os.path.join(PATH, 'README.md'), encoding='utf-8') as f:
     README = f.read()
 
+
+def req_from_file(filename):
+    """
+    Return a list of requirements, suitable for setup.py install, from a pip -r req.txt file
+    """
+    txt = open(filename, 'rb').read().decode('utf-8')
+    lines = re.split(r'[\n\r]+', txt)
+    return [line.split('#')[0].strip() for line in lines if line and line[0:2] != '-r']
+
+
 if __name__ == '__main__':
     setup(
         long_description=README,
         long_description_content_type='text/markdown',
         packages=find_packages(exclude=['contrib', 'docs', 'tests']),
         include_package_data=True,
+        install_requires=req_from_file(os.path.join(PATH, 'req/install.txt')),
         **CONFIG
     )
