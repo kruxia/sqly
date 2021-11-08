487475556cc649623b12c66031acde36ca8f1a98
Sean Harrison <sah@kruxia.com>
2021-11-07 19:26:32 -0600

    Database.connection_string_dialect - try to determine the dialect from the connection string

--- sqly/database.py
+++ sqly/database.py
@@ -1,12 +1,38 @@
+import logging
+
 from pydantic import BaseModel
 
 from sqly.dialect import Dialect
 from sqly.lib import run
 
+log = logging.getLogger('sqly.database')
+
 
 class Database(BaseModel):
     connection_string: str = ':memory:'
     dialect: Dialect = Dialect.SQLITE
+
+    @classmethod
+    def connection_string_dialect(cls, connection_string):
+        dialect = None
+        if connection_string == ':memory:':
+            dialect = Dialect.SQLITE
+
+        if connection_string.startswith('postgres'):
+            for dialect in [Dialect.ASYNCPG, Dialect.PSYCOPG2, Dialect.POSTGRES]:
+                try:
+                    dialect.load_adaptor()
+                    break
+                except Exception:
+                    pass
+
+        if connection_string.startswith('mysql'):
+            dialect = Dialect.MYSQL
+
+        if not dialect:
+            dialect = Dialect.PYODBC
+
+        return dialect
 
     def connect(self):
         mod = self.dialect.load_adaptor()
