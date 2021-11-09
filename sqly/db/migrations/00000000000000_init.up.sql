46e5803a116eea1d195cb83c0cdd0d540017b443
Sean Harrison <sah@kruxia.com>
2021-11-08 20:26:32 -0600

    delete sqly.schema and the associated db/migrations

--- sqly/db/migrations/00000000000000_init.up.sql
+++
@@ -1,9 +0,0 @@
--- 
-create table __migrations (
-    mod         varchar not null,
-    name        varchar not null,
-    PRIMARY KEY (mod, name),
-
-    requires    varchar[],
-    run_at      timestamptz default current_timestamp
-);
