c7ff59effe1ea723951722195575768d2bca5371
Sean Harrison <sah@kruxia.com>
2023-07-26 08:25:41 -0500

    setup.py [docs] includes [migration]

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ if __name__ == "__main__":
                 "twine~=4.0.2",
             ],
             "docs": [
+                "sqly[migration]",
                 "mkdocs~=1.4.3",
                 "mkdocs-autorefs~=0.4.1",
                 "mkdocs-click~=0.8.0",
